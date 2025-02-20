92a816f5669cb0837bfb54a4199e24b087ad2de6
pepedd864 <pepedd@qq.com>
2025-02-20 14:19:42 +0800

    chore: 设置打包分析

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [
+		react(),
+		visualizer({
+			filename: "bundle-analysis.html", // 分析报告输出文件名
+			gzipSize: true, // 显示 gzip 后的大小
+			brotliSize: true, // 显示 brotli 后的大小
+		}),
+	],
+});
